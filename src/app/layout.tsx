23d991ca0cc4ecfa07117126dbdd7bff7a99f043
Rachel Opperman <oppermanre17@gmail.com>
2025-11-01 17:13:21 -0400

    style: add light rays to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import './globals.css';
+
 import type { Metadata } from 'next';
 import { Martian_Mono, Schibsted_Grotesk } from 'next/font/google';
-import './globals.css';
 import type { ReactNode } from 'react';
+import LightRays from '@/components/LightRays';
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: '--font-schibsted-grotesk',
@@ -28,7 +30,21 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} antialiased min-h-screen`}
       >
-        {children}
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0.0}
+            distortion={0.01}
+          />
+        </div>
+
+        <main>{children}</main>
       </body>
     </html>
   );
