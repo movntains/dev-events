d5908a72070de0a17c69cd151767e31ae0d1488a
Rachel Opperman <oppermanre17@gmail.com>
2025-11-01 19:03:02 -0400

    feat(home): add featured events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,48 @@
 import Image from 'next/image';
+import Link from 'next/link';
+import EventCard from '@/components/EventCard';
+import { events } from '@/lib/constants';
 
-export default function Home() {
+export default function page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{' '}
-            or the{' '}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{' '}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <section className="space-y-20">
+      <div className="flex flex-col items-center gap-y-7">
+        <h1 className="flex flex-col gap-2 text-center">
+          <span>The Hub for Every Dev</span>
+          <span>Event You Can't Miss</span>
+        </h1>
+
+        <p className="text-center">
+          Hackathons, meetups, and conferences &mdash; all in one place.
+        </p>
+
+        <Link
+          href="#events"
+          className="flex justify-center items-center gap-2 border border-dark-200 bg-dark-100 w-fit cursor-pointer rounded-full px-8 py-3.5 max-sm:w-full text-center"
+        >
+          <span>Explore Events</span>
+
+          <Image
+            src="/icons/arrow-down.svg"
+            alt="arrow-down"
+            aria-hidden="true"
+            width={25}
+            height={24}
+          />
+        </Link>
+      </div>
+
+      <div className="space-y-7">
+        <h2>Featured Events</h2>
+
+        <ul className="grid gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard event={event} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
---
+++ src/components/EventCard.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import type { EventItem } from '@/types/event-item';
+
+interface EventCardProps {
+  event: EventItem;
+}
+
+export default function EventCard({ event }: EventCardProps) {
+  const { date, image, location, slug, time, title } = event;
+
+  return (
+    <Link
+      href={`/events/${slug}`}
+      className="flex flex-col gap-3"
+    >
+      <Image
+        src={image}
+        alt={title}
+        width={410}
+        height={300}
+        className="w-full h-[300px] rounded-lg object-cover"
+      />
+
+      <div className="flex flex-row gap-2">
+        <Image
+          src="/icons/pin.svg"
+          alt="location"
+          aria-hidden="true"
+          width={14}
+          height={14}
+        />
+
+        <p className="text-light-200 text-sm font-light">{location}</p>
+      </div>
+
+      <p className="text-[20px] font-semibold line-clamp-1">{title}</p>
+
+      <div className="text-light-200 flex flex-row flex-wrap items-center gap-4">
+        <div className="flex flex-row gap-2">
+          <Image
+            src="/icons/calendar.svg"
+            alt="date"
+            aria-hidden="true"
+            width={14}
+            height={14}
+          />
+
+          <p className="text-light-200 text-sm font-light">{date}</p>
+        </div>
+
+        <div className="flex flex-row gap-2">
+          <Image
+            src="/icons/clock.svg"
+            alt="time"
+            aria-hidden="true"
+            width={14}
+            height={14}
+          />
+
+          <p className="text-light-200 text-sm font-light">{time}</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
