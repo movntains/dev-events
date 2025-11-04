0f5368b6e39cf1346c20bcb4395457ae58fc74fa
Rachel Opperman <oppermanre17@gmail.com>
2025-11-03 23:24:15 -0500

    chore: remove leftover console log

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,7 +5,6 @@ interface ErrorPageProps {
 }
 
 export default function ErrorPage({ error }: ErrorPageProps) {
-  console.log(error);
   return (
     <div className="container flex flex-col items-center justify-center min-h-[50vh] gap-4">
       <h1 className="text-2xl font-bold">Something went wrong!</h1>
